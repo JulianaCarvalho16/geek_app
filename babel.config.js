10cbba525a387ffdc947209aca481d226d413dc6
GMFvx <gleidsonfvx@gmail.com>
2025-05-27 21:52:34 -0300

    tema escuro

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    'nativewind/babel',
+  ],
   plugins: [
     ['@babel/plugin-transform-class-properties', { loose: false }],
     ['@babel/plugin-transform-private-methods', { loose: false }],
     ['@babel/plugin-transform-private-property-in-object', { loose: false }],
-    'react-native-reanimated/plugin', // Mantenha sempre por último, como recomendado
+    'react-native-reanimated/plugin', // Mantenha sempre por último
   ],
 };
